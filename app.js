95be5e3191debce5e00a30995e66e357659b33b0
Som's Laptop <somaiahuthappa@gmail.com>
2022-02-22 17:46:55 -0500

    added HTML template

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
 
 
-const printProfileData = profileDataArr => {
-    // This
-    for (let i=0; i<profileDataArr.length; i++){
-        console.log(profileDataArr[i]);
-    }    
-    console.log('============');
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
 
-    // Is the same as this...
-    profileDataArr.forEach(profileItem =>  console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
